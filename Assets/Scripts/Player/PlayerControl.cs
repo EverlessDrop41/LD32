81b1b45efaff4e8f980fd426d7bde550b6ae3bb5
Thomas Peregrine <tomfperegrine@googlemail.com>
2015-04-19 16:29:08 +0100

    Added Raging, which lowers health

--- Assets/Scripts/Player/PlayerControl.cs
+++ Assets/Scripts/Player/PlayerControl.cs
@@ -1,15 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerControl : MonoBehaviour {
 
 	public Vector2 Speed;
-	
+    public int Health = 5;
+
+    public Text HealthDisplay;
+
 	private Rigidbody2D RB;
 	
 	void Start() {
 		RB = GetComponent<Rigidbody2D> ();
+
+        if (Health <= 0)
+        {
+            Debug.LogError("Health is too small - PlayerControl.cs");
+            HealthDisplay.text = string.Format("Health: ERROR");
+        }
+        else
+        {
+            HealthDisplay.text = string.Format("Health: {0}", Health);
+        }
 	}
 	
 	void Update () {
@@ -25,5 +39,28 @@ public class PlayerControl : MonoBehaviour {
 		float rotZ = Mathf.Atan2 (dir.x, dir.y) * Mathf.Rad2Deg;
 		
 		RB.MoveRotation (-rotZ);
+
+        if (Health < 0)
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+        else
+        {
+            HealthDisplay.text = string.Format("Health: {0}", Health);
+        }
 	}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.collider.gameObject.tag == "Enemy")
+        {
+            EnemyBehaviour enemy = coll.collider.gameObject.GetComponent<EnemyBehaviour>();
+            if (enemy.EnemyType == EnemyTypes.Brute || enemy.isRaging())
+            {
+                //Destroy(this.gameObject);
+                //Application.LoadLevel(Application.loadedLevel);
+                Health--;
+            }
+        }
+    }
 }
